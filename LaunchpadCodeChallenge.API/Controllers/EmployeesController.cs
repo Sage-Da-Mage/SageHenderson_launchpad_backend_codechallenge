9c8595e86db2c4689cbdbf5174d47e4c210f396a
sage henderson <2sagehenderson@gmail.com>
2021-12-14 01:15:38 -0700

    Added the Create, Update, Get and Delete endpoints to the EmployeeController.

--- LaunchpadCodeChallenge.API/Controllers/EmployeesController.cs
+++ LaunchpadCodeChallenge.API/Controllers/EmployeesController.cs
@@ -23,6 +23,65 @@ namespace LaunchpadCodeChallenge.API.Controllers
         }
 
 
+
+        // Creates a Employee from inputed data.
+        [HttpPost]
+        public async Task<ActionResult<EmployeeVM>> Create([FromBody] EmployeeCreateVM data)
+        {
+            // Have the service create the new Listing
+            var result = await _employeeService.Create(data);
+
+            //Return as 200 message along with the EmployeeVM
+            return Ok(result);
+        }
+
+        // Get a Employee by it's Id.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeVM>> Get([FromRoute] int id)
+        {
+            // Get the requested Listing entity from the service
+            var result = await _employeeService.Get(id);
+
+            // Return a 200 message with the ListingVM for the client
+            return Ok(result);
+        }
+
+
+        // Update a Employee using inputted data, excluding non-updatable properties
+        // like id.
+        [HttpPut("employee/{id}")]
+        public async Task<ActionResult<EmployeeVM>> Update([FromBody] EmployeeUpdateVM data)
+        {
+            // Update Listing entity from the service layer
+            var result = await _employeeService.Update(data);
+
+            // Return a 200 code + the ListingVM to comfirm the update went through properly
+            return Ok(result);
+        }
+
+
+        // Delete a listing from the database (determined by Id).
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] int id)
+        {
+            // Tell the repository to delete the requested Listing
+            await _employeeService.Delete(id);
+
+            // Return a 200 response to confirm it has completed
+            return Ok();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
         // Get all employees (CC required)
         [HttpGet]
         public async Task<List<EmployeeVM>> GetAll()
