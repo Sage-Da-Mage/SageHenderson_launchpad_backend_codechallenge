5699d50b2d26ed4a57283c70b777fdefdd37b448
sage henderson <2sagehenderson@gmail.com>
2021-12-14 06:55:16 -0700

    Completed Question 8, the single switch statement will provide the specified changes to a passed in object as desired by the Coding Challenge requirements.

---
+++ Question8Project/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Question8Project
+{
+    class Program
+    {
+        public static object TESTModule(object inputObject)
+        {
+            // Set result to null so that it can be checked through the switch
+            object result = null;
+
+            // Begin the switch statement
+            switch (inputObject)
+            {
+                // In the case where the input is between 1 and 4, double its value and return it
+                case int n when (n >= 1 && n <= 4):
+                    result = n * 2;
+                    break;
+
+                // When the input is an integer greater than 4, return its value threefold
+                case int n when (n > 4):
+                    result = n * 3;
+                    break;
+
+                // In the situation where the input is less than 1 throw an exception
+                case int n when (n < 1):
+                    throw new Exception("Numeric input should be higher than 1");
+
+                // When the input is a float of 1.0 or 2.0 return 3.0
+                case float n when (n == 1.0f || n == 2.0f):
+                    result = 3.0f;
+                    break;
+
+                // When the input is a string, uppercase it and return the result
+                case string itIsString:
+                    result = itIsString.ToUpper();
+                    break;
+                
+                // In all other circumstances, return what was passed in
+                default:
+                    result = inputObject;
+                    break;
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            object myInputObject = "Convert this string into uppercase letters";
+            Console.WriteLine("The TESTModule will return a value depending on what you change myInputObject to be: the current input is: " + myInputObject + " and it's current output is: " + TESTModule(myInputObject));
+
+        }
+    }
+}
